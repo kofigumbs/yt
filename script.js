32833aab96eaaf2481693e8f538b5513de9e4e53
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-03-28 18:30:27 -0400

    Flesh out instructions and usage rules

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-const search = document.querySelector(".search input");
 const params = new URLSearchParams(location.search);
+const search = document.querySelector(".search input");
+const inputs = document.querySelectorAll(".controls input");
+const controls = document.querySelectorAll(".controls nav");
+const holding = {};
+const keys = [ "a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j" ];
+
 search.value = params.get("id");
 
 function onYouTubeIframeAPIReady() {
   new YT.Player("player", {
-    events: { onReady },
+    events: { onStateChange, onReady },
     videoId: params.get("id") || search.placeholder,
     playerVars: { disablekb: 1, modestbranding: 1 },
   });
 }
 
+function onStateChange(event) {
+  if (event.data === YT.PlayerState.PLAYING) {
+    // Reset default times, evenly spaced throughout video
+    const unit = event.target.getDuration() / 12;
+    inputs.forEach((x, i) => x.placeholder = (i * unit).toFixed(1));
+  }
+}
+
 function onReady(event) {
-  loadVideo(event.target);
-  loadControls(event.target);
-  setupMidiListeners(event.target);
-}
+  const player = event.target;
+  const buffer = document.querySelector(".buffer");
+  startBuffering(player);
+  setInterval(() => buffer.innerText = (player.getVideoLoadedFraction() * 100).toFixed(0), 1000);
 
-// It seems there's no explicit way to load a video,
-// so instead we mute the video and start playing it immediately.
-// YouTube will lazily load the video as it plays.
-function loadVideo(player) {
-  player.mute();
-  player.playVideo();
-}
-
-const inputs = document.querySelectorAll(".controls input");
-function loadControls(player) {
-  const unit = player.getDuration() / 12;
-  inputs.forEach((x, i) => x.placeholder = (i * unit).toFixed(1));
+  // Update URL and video on search change
   search.addEventListener("input", function(event) {
-    const id = event.target.value || event.target.placeholder;
-    player.loadVideoById(id);
-    params.set("id", id);
+    player.loadVideoById(search.value || search.placeholder);
+    startBuffering(player);
+    params.set("id", search.value);
     window.history.replaceState(null, null, "?" + params.toString());
   });
-}
 
-function setupMidiListeners(player) {
+  // MIDI listeners
   window.addEventListener("keydown", event => onFakeMidi(event, player, 144, 100));
   window.addEventListener("keyup", event => onFakeMidi(event, player, 128, 0));
   window.navigator
@@ -47,15 +48,21 @@ function setupMidiListeners(player) {
     });
 }
 
+// It seems there's no explicit way to load a video,
+// so instead we mute the video and start playing it immediately.
+// YouTube will lazily load the video as it plays.
+function startBuffering(player) {
+  player.mute();
+  player.playVideo();
+}
+
 // Use the keyboard as a backup MIDI controller — GarageBand layout
-const keys = [ "a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j"];
 function onFakeMidi(event, player, status, velocity) {
   const index = keys.indexOf(event.key);
-  if (index !== -1 && !event.repeat)
+  if (index !== -1 && !event.repeat && event.target.nodeName !== "INPUT")
     onMidi(player, [ status, index, velocity ], performance.now());
 }
 
-const holding = {};
 function hold(i, toggle) {
   const note = i % 12;
   holding[note] = toggle;
@@ -63,7 +70,6 @@ function hold(i, toggle) {
 }
 
 // Make it a playable instrument!
-const controls = document.querySelectorAll(".controls nav");
 function onMidi(player, data, time) {
   if (data.length === 3 && data[0] >> 4 === 8) {
     // NOTE OFF
