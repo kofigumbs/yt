0e1e8a4bbfbf086bfc8d98585a3fb0aeb5649830
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-03-28 21:58:47 -0400

    Every app needs a logo

--- deploy.rb
+++ deploy.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-Dir.mkdir "build"
 File.write "build/index.html", File.read("index.html")
   .sub(/<link.+\/style.css.+$/,   "<style>#{ File.read "style.css" }</style>")
   .sub(/<script.+\/script.js.+$/, "<script>#{ File.read "script.js" }</script>")
