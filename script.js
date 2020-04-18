7ecf91acd2b9ec6b2b9176f9d9fb92b8e3e7371c
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-04-18 15:23:21 -0400

    More semantic controls

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const params = new URLSearchParams(location.search);
 const voices = document.getElementById("voices");
 const player = document.getElementById("player").parentNode;
 const search = document.getElementById("search");
-const inputs = document.querySelectorAll("#controls input");
-const controls = document.querySelectorAll("#controls nav");
 const holding = {};
-const keys = [ "a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j" ];
+
+// Controls object can be accessed by note index (0-11) or note key
+const controls = [ "a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j" ].reduce(setupControl, {});
+function setupControl(object, key, index) {
+  const container = document.querySelector("[data-key=" + key + "]");
+  const input = container.querySelector("input");
+  input.addEventListener("input", () => setParam(key, input.value));
+
+  const control = { key, index, container, input };
+  object[key] = control;
+  object[index] = control;
+  return object;
+}
 
 search.value = params.get("id");
 voices.addEventListener("change", function() {
@@ -14,12 +24,6 @@ voices.addEventListener("change", function() {
       .getElementById(option.value)
       .classList.toggle("dn", !option.selected);
 });
-
-for (const i of inputs) {
-  i.addEventListener("input", function() {
-    setParam(i.labels[0].innerText.replace("#", "s"), i.value);
-  });
-}
 
 function onYouTubeIframeAPIReady() {
   new YT.Player("player", {
@@ -35,7 +39,8 @@ function onStateChange(event) {
   if (event.data === YT.PlayerState.PLAYING) {
     // Reset default times, evenly spaced throughout video
     const unit = event.target.getDuration() / 12;
-    inputs.forEach((x, i) => x.placeholder = (i * unit).toFixed(1));
+    for (const control of Object.values(controls))
+      control.input.placeholder = (control.index * unit).toFixed(1);
   }
 }
 
@@ -79,9 +84,9 @@ function startBuffering(video) {
 
 // Use the keyboard as a backup MIDI controller — GarageBand layout
 function onFakeMidi(event, video, status, velocity) {
-  const index = keys.indexOf(event.key);
-  if (index !== -1 && !event.repeat && event.target.nodeName !== "INPUT")
-    onMidi(video, [ status, index, velocity ], performance.now());
+  const control = controls[event.key];
+  if (control && !event.repeat && event.target.nodeName !== "INPUT")
+    onMidi(video, [ status, control.index, velocity ], performance.now());
 }
 
 function hold(i, toggle) {
@@ -95,15 +100,15 @@ function onMidi(video, data, time) {
   if (data.length === 3 && data[0] >> 4 === 8) {
     // NOTE OFF
     const note = hold(data[1], false);
-    controls[note].classList.remove("pressed");
+    controls[note].container.classList.remove("pressed");
     if (!Object.values(holding).includes(true))
       video.mute();
 
   } else if (data.length === 3 && data[0] >> 4 === 9) {
     // NOTE ON
     const note = hold(data[1], true);
-    controls[note].classList.add("pressed");
-    video.seekTo(getValue(inputs[note]), true);
+    controls[note].container.classList.add("pressed");
+    video.seekTo(getValue(controls[note].input), true);
     video.unMute();
     video.playVideo();
   }
